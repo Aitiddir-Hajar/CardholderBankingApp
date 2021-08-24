473efc405d673c8578730697e6b7b2cceaa86a6f
Aitiddir-Hajar <65742385+Aitiddir-Hajar@users.noreply.github.com>
2021-08-24 15:25:29 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebaseConfig : {
-    apiKey: "AIzaSyBwZVfJam89ahFc6hZF_4TsodXnBRQI9A8",
-    authDomain: "fireproject-6b89a.firebaseapp.com",
-    databaseURL: "https://fireproject-6b89a.firebaseio.com",
-    projectId: "fireproject-6b89a",
-    storageBucket: "fireproject-6b89a.appspot.com",
-    messagingSenderId: "703767606069",
-    appId: "1:703767606069:web:e87699f9bf1f40ea6e127c"
+    apiKey: "xxx",
+    authDomain: "xxx",
+    databaseURL: "xxx",
+    projectId: "xxx",
+    storageBucket: "xxx",
+    messagingSenderId: "xxx",
+    appId: "xxx"
   }
 };
